a5fb0c18b20f2aa49c6235a73a7c0d1cad3821c5
Adriano Silva <adrsilva613@gmail.com>
2024-05-27 20:32:56 -0300

    [API] Criação das tabelas necessárias do modulo de manutenção

---
+++ prisma/migrations/20240527233143_create_tables_maintenance/migration.sql
@@ -0,0 +1,108 @@
+-- CreateTable
+CREATE TABLE `equipamentos` (
+    `id` VARCHAR(191) NOT NULL,
+    `codigo` VARCHAR(255) NOT NULL,
+    `nome` VARCHAR(255) NOT NULL,
+    `especificacao` LONGTEXT NULL,
+    `frequecia` INTEGER NOT NULL DEFAULT 0,
+    `empresaId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `pecas_equipamento` (
+    `id` VARCHAR(191) NOT NULL,
+    `nome` VARCHAR(255) NOT NULL,
+    `descricao` MEDIUMTEXT NULL,
+    `equipamentoId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `agenda_inspecao_equipamentos` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `agendadoPara` DATE NOT NULL,
+    `equipamentoId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `historico_equipamento` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `data` DATETIME(3) NOT NULL,
+    `descricao` TEXT NOT NULL,
+    `equipamentoId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `manutencoes` (
+    `id` VARCHAR(191) NOT NULL,
+    `criadoEm` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `iniciadoEm` DATETIME(3) NULL,
+    `finalizadoEm` DATETIME(3) NULL,
+    `canceladoEm` DATETIME(3) NULL,
+    `equipamentoId` VARCHAR(191) NOT NULL,
+    `usuarioId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `inspecao_equipamentos` (
+    `id` VARCHAR(191) NOT NULL,
+    `iniciadoEm` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `finalizadoEm` DATETIME(3) NULL,
+    `equipamentoId` VARCHAR(191) NOT NULL,
+    `usuarioId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `pontos_inspecao_equipamento` (
+    `inspecaoId` VARCHAR(191) NOT NULL,
+    `equipamentoId` VARCHAR(191) NOT NULL,
+    `pecaEquipamentoId` VARCHAR(191) NOT NULL,
+    `aprovado` BOOLEAN NOT NULL DEFAULT false,
+    `inspecionadoEm` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`inspecaoId`, `equipamentoId`, `pecaEquipamentoId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `equipamentos` ADD CONSTRAINT `equipamentos_empresaId_fkey` FOREIGN KEY (`empresaId`) REFERENCES `empresas`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `pecas_equipamento` ADD CONSTRAINT `pecas_equipamento_equipamentoId_fkey` FOREIGN KEY (`equipamentoId`) REFERENCES `equipamentos`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `agenda_inspecao_equipamentos` ADD CONSTRAINT `agenda_inspecao_equipamentos_equipamentoId_fkey` FOREIGN KEY (`equipamentoId`) REFERENCES `equipamentos`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `historico_equipamento` ADD CONSTRAINT `historico_equipamento_equipamentoId_fkey` FOREIGN KEY (`equipamentoId`) REFERENCES `equipamentos`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `manutencoes` ADD CONSTRAINT `manutencoes_equipamentoId_fkey` FOREIGN KEY (`equipamentoId`) REFERENCES `equipamentos`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `manutencoes` ADD CONSTRAINT `manutencoes_usuarioId_fkey` FOREIGN KEY (`usuarioId`) REFERENCES `usuarios`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `inspecao_equipamentos` ADD CONSTRAINT `inspecao_equipamentos_equipamentoId_fkey` FOREIGN KEY (`equipamentoId`) REFERENCES `equipamentos`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `inspecao_equipamentos` ADD CONSTRAINT `inspecao_equipamentos_usuarioId_fkey` FOREIGN KEY (`usuarioId`) REFERENCES `usuarios`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `pontos_inspecao_equipamento` ADD CONSTRAINT `pontos_inspecao_equipamento_inspecaoId_fkey` FOREIGN KEY (`inspecaoId`) REFERENCES `inspecao_equipamentos`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `pontos_inspecao_equipamento` ADD CONSTRAINT `pontos_inspecao_equipamento_equipamentoId_fkey` FOREIGN KEY (`equipamentoId`) REFERENCES `equipamentos`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `pontos_inspecao_equipamento` ADD CONSTRAINT `pontos_inspecao_equipamento_pecaEquipamentoId_fkey` FOREIGN KEY (`pecaEquipamentoId`) REFERENCES `pecas_equipamento`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
